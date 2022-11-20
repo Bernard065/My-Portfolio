d3b86ffa2fd39975f51a850b8f6a272931262c17
Bernard065 <bebenibernard@gmail.com>
2022-11-20 20:15:47 +0300

    Portfolio Services

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-//const h2 = document.createElement("h2");
-//h2.textContent = "This content added by JavaScript";
-//document.querySelector("body").appendChild(h2);
+let menu = document.querySelector('.menu-icon');
+
+menu.onclick = () => {
+    menu.classList.toggle("move");
+};
